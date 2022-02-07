4747e4576dc6558aef4e82f0e2399666218eeff7
damladlg <damlabileydi@gmail.com>
2022-02-07 11:31:27 +0300

    created def request

--- yeba3/main.py
+++ yeba3/main.py
@@ -6,28 +6,30 @@ from datetime import datetime
 import schedule
 import time
 
-start_date="06.02.2022%2014:00:00"
-end_date="06.02.2022%2015:00:00"  
+def request_to_url(start_date,end_date):
+    parameters = {
+        "StationId": "377e1216-bcc7-42c0-aad8-4d5b3d602b78",
+        "StartDate": start_date,
+        "EndDate": end_date
+        }
+    payload = {}
+    headers= {}
+    url="https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIByStationId"
 
-parameters = {
-"StationId": "377e1216-bcc7-42c0-aad8-4d5b3d602b78",
-"StartDate": start_date,
-"EndDate": end_date
-}
-payload = {}
-headers= {}
-url="https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIByStationId"
-
-istek = requests.get(url, params = parameters)
-if istek.status_code == 200:
-    decodeUrl = unquote(istek.url) #invalid degerlerden dolayi url decode ediliyor.
-
-    response = requests.get(decodeUrl,headers=headers, data = payload)
-    result = json.loads(response.text)
-    print("İstek basariyla atildi. Json veriler cekildi.")
-else:
-    print("İstek karsilanamadi.")
+    istek = requests.get(url, params = parameters)
+    if istek.status_code == 200:
+        decodeUrl = unquote(istek.url) #invalid degerlerden dolayi url decode ediliyor.
+        response = requests.get(decodeUrl,headers=headers, data = payload)
+        result = json.loads(response.text) 
+        print("İstek basariyla atildi. Json veriler cekildi.")
+        return result
+    else:
+        print("İstek karsilanamadi.")
     
+start_date="06.02.2022%2017:00:00"
+end_date="06.02.2022%2018:00:00"      
+insert_data=request_to_url(start_date,end_date)
+
 database_name="postgres"
 user_name="postgres"
 password="Tamam123"
@@ -84,7 +86,7 @@ db.autocommit=True
 cursor=db.cursor()
 cursor.execute(query_create_table)
 
-for i in result:
+for i in insert_data:
     query_insert_table="""INSERT into hava_kalitesi(ReadTime, Concentration_PM10, Concentration_SO2, Concentration_O3, Concentration_NO2, Concentration_CO, AQI_PM10, AQI_SO2, AQI_O3, AQI_NO2, AQI_CO, AQI_AQIIndex, AQI_ContaminantParameter, AQI_State, AQI_Color) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
     record_to_insert = (i['ReadTime'], i['Concentration']['PM10'], i['Concentration']['SO2'],i['Concentration']['O3'],
                         i['Concentration']['NO2'],i['Concentration']['CO'],i['AQI']['PM10'],i['AQI']['SO2'],
@@ -93,41 +95,26 @@ for i in result:
     cursor.execute(query_insert_table, record_to_insert)
 
 def job():
+    record_to_insert_hourly = tuple()
     now = datetime.now()
     date_string = now.strftime("%d.%m.%Y")
     time_string = now.strftime("%H:00:00")
     start_date=date_string+"%20"+time_string
     end_date=date_string+"%20"+time_string
-    parameters = {
-    "StationId": "377e1216-bcc7-42c0-aad8-4d5b3d602b78",
-    "StartDate": start_date,
-    "EndDate": end_date
-    }
-    payload = {}
-    headers= {}
-    url="https://api.ibb.gov.tr/havakalitesi/OpenDataPortalHandler/GetAQIByStationId"
-
-    istek = requests.get(url, params = parameters)
-    if istek.status_code == 200:
-        decodeUrl = unquote(istek.url) #invalid degerlerden dolayi url decode ediliyor.
-
-        response = requests.get(decodeUrl,headers=headers, data = payload)
-        hourly_result = json.loads(response.text)
-        for i in hourly_result:
-            query_insert_table_hourly="""INSERT into hava_kalitesi(ReadTime, Concentration_PM10, Concentration_SO2, Concentration_O3, Concentration_NO2, Concentration_CO, AQI_PM10, AQI_SO2, AQI_O3, AQI_NO2, AQI_CO, AQI_AQIIndex, AQI_ContaminantParameter, AQI_State, AQI_Color) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-            record_to_insert_hourly = (i['ReadTime'], i['Concentration']['PM10'], i['Concentration']['SO2'],i['Concentration']['O3'],
-                                i['Concentration']['NO2'],i['Concentration']['CO'],i['AQI']['PM10'],i['AQI']['SO2'],
-                                i['AQI']['O3'], i['AQI']['NO2'],i['AQI']['CO'], i['AQI']['AQIIndex'], i['AQI']['ContaminantParameter'],
-                                i['AQI']['State'],i['AQI']['Color'])
-            cursor.execute(query_insert_table_hourly, record_to_insert_hourly)
-            print("Tabloya yazdı.")
-        print("Saatlik istek atildi.")
-        hourly_result.clear()
-    else:
-        print("İstek karsilanamadi.")
+    
+    hourly_result=request_to_url(start_date,end_date)
+     
+    query_insert_table_hourly="""INSERT into hava_kalitesi(ReadTime, Concentration_PM10, Concentration_SO2, Concentration_O3, Concentration_NO2, Concentration_CO, AQI_PM10, AQI_SO2, AQI_O3, AQI_NO2, AQI_CO, AQI_AQIIndex, AQI_ContaminantParameter, AQI_State, AQI_Color) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    record_to_insert_hourly = (hourly_result[0]['ReadTime'], hourly_result[0]['Concentration']['PM10'], hourly_result[0]['Concentration']['SO2'],hourly_result[0]['Concentration']['O3'],
+                        hourly_result[0]['Concentration']['NO2'],hourly_result[0]['Concentration']['CO'],hourly_result[0]['AQI']['PM10'],hourly_result[0]['AQI']['SO2'],
+                        hourly_result[0]['AQI']['O3'], hourly_result[0]['AQI']['NO2'],hourly_result[0]['AQI']['CO'], hourly_result[0]['AQI']['AQIIndex'], hourly_result[0]['AQI']['ContaminantParameter'],
+                        hourly_result[0]['AQI']['State'],hourly_result[0]['AQI']['Color'])
+    cursor.execute(query_insert_table_hourly, record_to_insert_hourly)
+    hourly_result.clear()
 
 schedule.every().hour.do(job)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+    
