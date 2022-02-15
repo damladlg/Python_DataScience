3e13469439f80f90d65c299bd737eba5dacbf927
damladlg <damlabileydi@gmail.com>
2022-02-15 15:31:46 +0300

    designed homepage

--- yeba3/arayuz/app.py
+++ yeba3/arayuz/app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/login")
 def login():
     return render_template("login.html")
-
-
-@app.route("/search")
-def search():
-    return "Search"
 
 
 if __name__ == "__main__":
