63dcdc3ee0044841cc12ef109128daa57210cd91
damladlg <damlabileydi@gmail.com>
2022-02-18 15:51:02 +0300

    connected database

--- yeba3/arayuz/app.py
+++ yeba3/arayuz/app.py
@@ -1,7 +1,36 @@
-from distutils.log import debug
+import os
+import psycopg2
 from flask import Flask, render_template
+from os.path import exists
 
 app = Flask(__name__)
+
+database_name = "yhk_db"
+user_name = "postgres"
+password = "Tamam123"
+host_ip = "127.0.0.1"
+host_port = "5432"
+
+db = psycopg2.connect(database=database_name,
+                      user=user_name,
+                      password=password,
+                      host=host_ip,
+                      port=host_port)
+
+db.autocommit = True
+cursor = db.cursor()
+
+
+@app.route('/report')
+def contacts():
+    try:
+        cursor.execute("""SELECT * from hava_kalitesi""")
+        rows = cursor.fetchall()
+
+        return render_template('report.html',  results=rows)
+    except Exception as e:
+        print(e)
+        return []
 
 
 @app.route("/")
