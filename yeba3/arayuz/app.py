f95c9dacc3faf76bf9f9f16365fb3abf1eb5b7af
damladlg <damlabileydi@gmail.com>
2022-02-19 22:31:53 +0300

    checked login page.

--- yeba3/arayuz/app.py
+++ yeba3/arayuz/app.py
@@ -1,6 +1,8 @@
+#from crypt import methods
+
 import os
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from os.path import exists
 
 app = Flask(__name__)
@@ -11,18 +13,20 @@ password = "Tamam123"
 host_ip = "127.0.0.1"
 host_port = "5432"
 
-db = psycopg2.connect(database=database_name,
+try: 
+    db = psycopg2.connect(database=database_name,
                       user=user_name,
                       password=password,
                       host=host_ip,
                       port=host_port)
+except (Exception, psycopg2.Error) as error:
+    print("Error while fetching data from PostgreSQL", error)
 
 db.autocommit = True
 cursor = db.cursor()
 
-
 @app.route('/report')
-def contacts():
+def report():
     try:
         cursor.execute("""SELECT * from hava_kalitesi""")
         rows = cursor.fetchall()
@@ -35,12 +39,32 @@ def contacts():
 
 @app.route("/")
 def homepage():
-    return render_template("homepage.html")
+    try:
+        cursor.execute("""SELECT * FROM hava_kalitesi ORDER BY id DESC LIMIT 1""")
+        rows = cursor.fetchall()
+
+        return render_template('homepage.html',  results=rows)
+    except Exception as e:
+        print(e)
+        return []
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
-    return render_template("login.html")
+    cursor.execute("""SELECT * from user_login""")
+    rows = cursor.fetchall()
+    for row in rows:
+        email=row[1]
+        password=row[2]
+    if request.method=="POST":
+        print(request.form['email'])
+        if email==request.form['email'] and password==request.form['password']:
+            print("email dogru")
+            return redirect(url_for('report'))
+        else:
+            return redirect(url_for('login'))
+    
+    return render_template('login.html',  results=rows)
 
 
 if __name__ == "__main__":
